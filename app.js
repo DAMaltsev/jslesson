9c4ae7d6cbcc06ce39a1a54f0b1ee3c43d8855ec
DAMaltsev <daniil.malcev@gmail.com>
2021-04-08 00:37:02 +0500

    task 2

--- app.js
+++ app.js
@@ -25,4 +25,61 @@ console.log(answers);
 // `бэктик позволяет использовать интерполяцию (ES6) - вставить переменную внутрь строки - ${}(Шаблонные строки)
 
 const category = 'reading';
+
 console.log(`www.google.com/${category}/5`);
+
+//В JS есть тернарные операторы 
+
+const num = 50;
+
+(num == 1) ? console.log('Ok!') : console.log('Error')
+
+//В JS есть так же бинарные (4+4) и унарные операторы (+4)
+//Существует конструкция SWITCH она сравнивает только на СТРОГОЕ значение
+
+const num2 = 1;
+
+switch(num2) {
+    case 3:
+        console.log('Неверно!');
+        break;
+    case 2:
+        console.log("Неверно");
+        break;
+    case 1:
+        console.log('Ок');
+        break;
+    default: 
+        console.log('Не в этот раз!')
+        break;
+}
+
+
+//Циклы while do/while for 
+
+let num3 = 10;
+
+while (num3 < 15 ) {
+    console.log(num3);
+    num3++;
+}
+
+let num4 = 10;
+
+do {
+    console.log(num4);
+    num4++
+}
+while ( num4 < 20);
+
+// continue пропускает значение и продолжает цикл. Break прерывает. 
+
+for (let i=1; i < 8; i++) {
+
+    if (i===6) {
+        // break;
+        continue;
+    }
+
+     console.log(i);
+}
--- task1.js
+++ task1.js
@@ -7,12 +7,77 @@ const personMovieDB = {
     pricate: false
 };
 
-const firstMovie = prompt('Один из последних просмотренных фильмов?', ''),
-    firstReview = prompt('На сколько его оцените?'),
-    secondMovie = prompt('Один из последних просмотренных фильмов?', ''),
-    secondReview = prompt('На сколько его оцените?');
+if (numverOfFilms < 10) {
+    console.log('Мало фильмов');
+} else if(numverOfFilms < 30 ){
+    console.log('Вы средний зритель');
+} else if(numverOfFilms > 30 ){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
-personMovieDB.movies[firstMovie] = firstReview;
-personMovieDB.movies[secondMovie] = secondReview;
+// const firstMovie = prompt('Один из последних просмотренных фильмов?', ''),
+//     firstReview = prompt('На сколько его оцените?'),
+//     secondMovie = prompt('Один из последних просмотренных фильмов?', ''),
+//     secondReview = prompt('На сколько его оцените?');
+
+// personMovieDB.movies[firstMovie] = firstReview;
+// personMovieDB.movies[secondMovie] = secondReview;
+
+
+
+
+// for (let i=0; i < 1; i++) {
+//     let Movie = prompt('Один из последних просмотренных фильмов?', ''),
+//         Review = prompt('На сколько его оцените?');
+
+//     if (Movie == "" ||  Review == 0 || Movie.length > 50) {
+//         console.log('Error');
+//         i=0;
+//         continue;
+//     } else {
+//         personMovieDB.movies[Movie] = Review;
+//     }    
+// }
+
+// let i = 0;
+
+// while (i < 2) {
+//     let Movie = prompt('Один из последних просмотренных фильмов?', ''),
+//         Review = prompt('На сколько его оцените?');
+//     if (Movie == "" ||  Review == 0 || Movie.length > 50) {
+//         console.log('Error');
+//     } else {
+//         personMovieDB.movies[Movie] = Review;
+//         i++;
+//     }    
+// }
+
+// do {
+//     let Movie = prompt('Один из последних просмотренных фильмов?', ''),
+//         Review = prompt('На сколько его оцените?');
+
+//     if (Movie == "" ||  Review == 0 || Movie.length > 50) {
+//         console.log('Error');
+//     } else {
+//         personMovieDB.movies[Movie] = Review;
+//         i++;
+//     }
+// }
+// while (i < 2);
+
+for (let i=0; i < 2; i++ ) {
+    let Movie = prompt('Один из последних просмотренных фильмов?', ''),
+         Review = prompt('На сколько его оцените?');
+
+    if (Movie == "" ||  Review == 0 || Movie == null ||  Review == null || Movie.length > 50) {
+        console.log('Error');
+        i--;
+        console.log(i);
+    } else {
+        personMovieDB.movies[Movie] = Review;
+    }
+}
 
 console.log(personMovieDB);
