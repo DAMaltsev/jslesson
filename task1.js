e5720fe3eb9c4cb5ee4eb1f21b4f0c3d089c0897
DAMaltsev <daniil.malcev@gmail.com>
2021-04-08 21:33:31 +0500

    third tast

--- task1.js
+++ task1.js
@@ -1,21 +1,58 @@
-const numverOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+let numverOfFilms;
+
+function start () {
+    numverOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+    while (numverOfFilms == "" || numverOfFilms == null || isNaN(numverOfFilms)) {
+        numverOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+    }
+}
+
+start();
+
 const personMovieDB = {
     count: numverOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    pricate: false
+    private: false
 };
 
-if (numverOfFilms < 10) {
-    console.log('Мало фильмов');
-} else if(numverOfFilms < 30 ){
-    console.log('Вы средний зритель');
-} else if(numverOfFilms > 30 ){
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function showMyDB() {
+    if (personMovieDB.private==false) {
+        console.log(personMovieDB);
+        console.log('Great work!');
+    } else {
+        console.log('error!');
+    }
 }
+
+showMyDB(personMovieDB);
+
+
+function writeGenres() {
+    for (let i = 1; i < 4; i++ ) {
+        personMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');
+    }
+}
+
+writeGenres();
+
+function detectLevel() {
+    if (numverOfFilms < 10) {
+        console.log('Мало фильмов');
+    } else if(numverOfFilms < 30 ){
+        console.log('Вы средний зритель');
+    } else if(numverOfFilms > 30 ){
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+// detectLevel();
+
+
 
 // const firstMovie = prompt('Один из последних просмотренных фильмов?', ''),
 //     firstReview = prompt('На сколько его оцените?'),
@@ -67,17 +104,24 @@ if (numverOfFilms < 10) {
 // }
 // while (i < 2);
 
-for (let i=0; i < 2; i++ ) {
-    let Movie = prompt('Один из последних просмотренных фильмов?', ''),
-         Review = prompt('На сколько его оцените?');
 
-    if (Movie == "" ||  Review == 0 || Movie == null ||  Review == null || Movie.length > 50) {
-        console.log('Error');
-        i--;
-        console.log(i);
-    } else {
-        personMovieDB.movies[Movie] = Review;
+function rememberMyFilms() {
+    for (let i=0; i < 2; i++ ) {
+        let Movie = prompt('Один из последних просмотренных фильмов?', ''),
+             Review = prompt('На сколько его оцените?');
+    
+        if (Movie == "" ||  Review == 0 || Movie == null ||  Review == null || Movie.length > 50) {
+            console.log('Error');
+            i--;
+            console.log(i);
+        } else {
+            personMovieDB.movies[Movie] = Review;
+        }
     }
 }
+
+// rememberMyFilms();
+
+
 
 console.log(personMovieDB);
